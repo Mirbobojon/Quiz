70cf87134314820b924badd34a3ad9a725b2a240
Mirbobojon <mirbobojon@gmail.com>
2022-06-24 22:51:05 +0500

    first commit

---
+++ src/components/Start.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Start = ({ onQuizStart }) => {
+  return(
+    <div className="card">
+      <div className="card-content">
+        <div className="content">
+          <h1>Testni Boshlash</h1>
+          <p>✊ Omad! ✊ </p>
+          <button className="button is-info is-medium" onClick={onQuizStart}>Boshla</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Start;
